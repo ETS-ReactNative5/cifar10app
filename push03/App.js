6500bcfac118007340c0e800e2d21db31dbf2fe4
BenSagir <bensagir.sbr@gmail.com>
2021-12-25 22:53:07 +0200

    stylling with linear gradient

--- push03/App.js
+++ push03/App.js
@@ -8,53 +8,48 @@
  */
 
 import React, {useState, useEffect} from 'react';
-import type {Node} from 'react';
 import {
   Dimensions,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   TouchableOpacity,
   Platform, View,
   Image,
 } from 'react-native';
-import Cifar from './cifar_test_data/cifar';
-import ImagePicker from 'react-native-image-picker'
-import Tflite from 'tflite-react-native'
+import LinearGradient from 'react-native-linear-gradient';
+import ImagePicker from 'react-native-image-picker';
+import Tflite from 'tflite-react-native';
 
-let tflite = new Tflite()
-var modelFile = 'models/CIFAR10_model.tflite'
-var labelsFile = 'models/CIFAR_labels.txt'
+let tflite = new Tflite();
+var modelFile = 'models/CIFAR10_model.tflite';
+var labelsFile = 'models/CIFAR_labels.txt';
 
 function App() {
-  const [recognition, setRecognition] = useState(null)
-  const [source, setSource] = useState()
-  const [input, setInput] = useState()
+  const [recognition, setRecognition] = useState(null);
+  const [source, setSource] = useState();
 
   function runModel(path) {
     tflite.runModelOnImage(
       {path: path}, (err, ress) => {
-        if (err) console.log(err);
+        if (err) {console.log(err);}
         else {
           console.log(ress);
-          setRecognition(ress[0]);
+          setRecognition(ress);
         }
       });
   }
   function selectGallaryImage() {
     const options = {};
     ImagePicker.launchImageLibrary(options, res => {
-      if (res.didCancel) console.log('caneled');
-      else if (res.error) console.log('error');
-      else if (res.customButton) console.log('custom butt');
+      if (res.didCancel) {console.log('caneled');}
+      else if (res.error) {console.log('error');}
+      else if (res.customButton) {console.log('custom butt');}
       else {
         console.log('Libaray open');
         var path = Platform.OS === 'ios' ? res.uri : 'file://' + res.path;
         setSource(path);
-        console.log(path)
-        runModel(path)
+        console.log('path is : ' + path);
+        runModel(path);
       }
     });
   }
@@ -64,37 +59,43 @@ function App() {
     tflite.loadModel(
       {model: modelFile, labels: labelsFile},
       (err, res) => {
-        if (err) console.log(err);
-        else console.log(res);
+        if (err) {console.log(err);}
+        else {console.log('useEffect respose: ' + res);}
       });
   }, []);
 
-  const AIR_PATH = './cifar_test_data/airplane1.png'
+
 
 
   return (
-    <View style={styles.main}>
-      <Text style={styles.text}>CIFAR10 app</Text>
+    <LinearGradient colors={['rgba(255, 255, 255, 0)', 'rgba(0, 0, 0, 0.8)']} style={styles.main}>
+      <LinearGradient colors={['rgba(0,0,0, 0.6)', 'rgba(255, 255, 255, 0)']} style={styles.header}>
+        <Text style={[styles.text, {fontSize: 24, color: '#fff'}]}>CIFAR10 Application</Text>
+        <Text style={styles.text}>This is an a presentetion of prediction of neural network that was train on CIFAR10 dataset</Text>
+
+      </LinearGradient>
+
 
       <TouchableOpacity style={styles.button} onPress={() => selectGallaryImage()}>
         <Text style={styles.WT}>Camera Roll</Text>
       </TouchableOpacity>
 
-      <Cifar setImage={item => setInput(item)} />
-      <TouchableOpacity style={styles.button} onPress={() => runModel('file://./cifar_test_data/airplane1.png')}>
-        <Text style={styles.WT}>from internet</Text>
-      </TouchableOpacity>
+
       {
         recognition ?
           <View>
-            <Image source={source}></Image>
-            <Text style={styles.text}>{recognition['label'] + ' - ' + (recognition['confidence'] * 100).toFixed(0) + '%'}</Text>
-          </View> : null
+            <Image source={source} />
+            {recognition.length === 1 ?
+              <Text style={[styles.text, {fontSize: 24, color: '#fff'}]}>first peak: {recognition[0].label + ' - ' + (recognition[0].confidence * 100).toFixed(0) + '%'}</Text>
+              :
+              <Text>sec</Text>
+            }
+          </View> : <View />
       }
 
-    </View >
+    </LinearGradient >
   );
-};
+}
 
 const styles = StyleSheet.create({
   main: {
@@ -104,10 +105,20 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     flex: 1,
-    width: Dimensions.get('screen').width
+    width: Dimensions.get('screen').width,
+  },
+  header: {
+    height: Dimensions.get('screen').height * 25 / 100,
+    backgroundColor: '#ef6',
+    padding: 10,
+    position: 'absolute',
+    top: 0,
+    width: Dimensions.get('screen').width,
+    borderBottomWidth: 3,
+    borderBottomColor: '#444'
   },
   button: {
-    backgroundColor: '#aaa',
+    backgroundColor: '#5dd',
     borderWidth: 5,
     padding: 30,
     alignItems: 'center',
@@ -115,9 +126,9 @@ const styles = StyleSheet.create({
   },
   WT: {
     color: '#fff',
-    fontSize: 23
+    fontSize: 23,
   },
-  text: {fontSize: 22, fontWeight: 'bold', margin: 10},
+  text: {fontSize: 16, fontWeight: 'bold', margin: 10, padding: 10},
 
 });
 
